995f960c241e69620e52b173d936736e934b13f0
Ramon Fernandez Mir <rfernandezmir@gmail.com>
2020-11-11 23:27:09 +0000

    Little progress with picard operator.

--- src/picard_lindelof/picard_operator.lean
+++ src/picard_lindelof/picard_operator.lean
@@ -1,8 +1,11 @@
 import analysis.calculus.mean_value
+import topology.continuous_map
 import measure_theory.interval_integral
 
 -- Banach fixed point theorem:
 -- https://github.com/leanprover-community/mathlib/blob/f25340175631cdc85ad768a262433f968d0d6450/src/topology/metric_space/lipschitz.lean#L110
+
+-- Following Imperial's MA2AA1 notes.
 
 noncomputable theory
 open metric set asymptotics filter real measure_theory interval_integral topological_space
@@ -15,21 +18,33 @@ variables {E : Type*} [normed_group E] [normed_space ℝ E]
           [complete_space E] [measurable_space E]
           [borel_space E]
 
--- 
+-- TODO: Should probably be from Icc a b. 
 variables {t0 : ℝ} {f : ℝ → E → E}
 
-def picard_operator (x : C(ℝ, E)) :=
-λ t, (x t0) + ∫ s in t0..t, f s (x s)
+def picard_operator_raw (x : C(ℝ, E)) :=
+λ t, (x t0) + ∫ s in t0..t, (f s (x s))
 
---instance : has_edist C(ℝ, E) :=
---⟨λ x y, ∣(x 0) - (y 0)∣⟩
+lemma picard_operator_raw_continuous 
+: ∀ (x : C(ℝ, E)), continuous (@picard_operator_raw E _ _ _ _ _ _ t0 f x) :=
+sorry
 
-instance : emetric_space C(ℝ, E) := sorry
+def picard_operator : C(ℝ, E) → C(ℝ, E) :=
+λ x, ⟨picard_operator_raw x, @picard_operator_raw_continuous E _ _ _ _ _ _ t0 f x⟩
+
+instance : has_edist C(ℝ, E) := sorry
+--⟨λ x y, supr (λ t, ∥(x t) - (y t)∥)⟩
+
+instance : metric_space C(ℝ, E) := sorry
+
+-- Ideally, we can show that it is a Banach space.
+--instance : normed_group C(Icc a b, E) := sorry
+--instance : normed_space ℝ C(Icc a b, E) := sorry
+--instance : complete_space ℝ C(Icc a b, E) := sorry
 
 variables (K : nnreal) (hK : K < 1)
 
 lemma picard_operator_lischitz 
-: ∀ t, lipschitz_with K (λ x, @picard_operator E _ _ _ _ _ _ t0 f x t)
-
+: lipschitz_with K (@picard_operator E _ _ _ _ _ _ t0 f) :=
+sorry 
 
 end picard_operator
