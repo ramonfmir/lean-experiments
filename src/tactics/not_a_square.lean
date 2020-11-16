7b54734b9d1b84f9a5a05310ce8e033ee22fffe9
Ramon Fernandez Mir <rfernandezmir@gmail.com>
2020-11-16 21:58:11 +0000

    Working not_a_square. Times out quickly.

--- src/tactics/not_a_square.lean
+++ src/tactics/not_a_square.lean
@@ -1,6 +1,5 @@
 import tactic
-import data.nat.basic 
-import data.zmod.basic
+import data.nat.basic
 
 namespace tactic.interactive
 
@@ -8,23 +7,24 @@ open tactic.interactive («have»)
 open tactic (get_local infer_type) 
 open lean.parser interactive interactive.types (texpr)
 
-meta def using_texpr := (tk "using" *> ident)
+meta def using_texpr := (tk "using" *> texpr)
 
 lemma le_of_mul_self_eq {n m : ℕ} (h : n = m * m) : m ≤ n := sorry
 
-meta def not_a_square (x : parse ident) (h : parse using_texpr) : tactic unit :=
-do {`(false) ← tactic.target,
-    xe ← get_local x,
-    he ← get_local h,
+meta def not_a_square_aux (x : parse texpr) (h : parse using_texpr) : tactic unit :=
+do {xe ← tactic.to_expr x,
+    he ← tactic.to_expr h,
     «have» ("hl" : name) none ``(zero_le %%xe),
     «have» ("hu" : name) none ``(le_of_mul_self_eq %%he),
-    `[interval_cases x; try {cases hx}] }
+    interval_cases x none none }
+
+meta def not_a_square : tactic unit :=
+do `[rintros ⟨x, hx⟩, not_a_square_aux x using hx; cases hx]
 <|> tactic.trace "Error"
 
-lemma three_not_a_square : ¬ ∃ x, 3 = x * x :=
-begin 
-    rintros ⟨x, hx⟩,
-    not_a_square x using hx,
-end 
+example : ¬ ∃ x, 3 = x * x := by not_a_square
+
+-- Timeout...
+example : ¬ ∃ x, 30 = x * x := by not_a_square
 
 end tactic.interactive
