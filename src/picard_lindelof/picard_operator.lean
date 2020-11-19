1bfaedce6c7ee45af352360d8c82875977de7bb4
Ramon Fernandez Mir <rfernandezmir@gmail.com>
2020-11-19 18:41:52 +0000

    In a good position to prove P.lipschitz_at_of_lipshitz.

--- src/picard_lindelof/picard_operator.lean
+++ src/picard_lindelof/picard_operator.lean
@@ -16,21 +16,22 @@ open_locale topological_space classical filter uniformity
 section picard_operator
 
 -- NOTE: This is meant to be [a, b].
-parameters {A : Type*} [topological_space A] [measurable_space A] [linear_order A]
+parameters {A : Type*} [nonempty A] [topological_space A] [measurable_space A] 
+                       [linear_order A]
 
 parameter (μ : measure A)
 
 -- NOTE: This is meant to be ℝ^n.
 parameters {B : Type*} [normed_group B] [normed_space ℝ B] [measurable_space B]
-                      [complete_space B] [second_countable_topology B] [borel_space B]
+                       [complete_space B] [second_countable_topology B] [borel_space B]
           
 
-parameters (t0 : A) (f : A → B → B)
+parameters (t0 : A) (x0 : B) (f : A → B → B)
 
 local infixr ` →ᵇ `:25 := bounded_continuous_function
 
 -- Picard operator as a function.
-def P_raw (x : A →ᵇ B) : A → B := λ t, (x t0) + ∫ s in t0..t, (f s (x s)) ∂μ
+def P_raw (x : A →ᵇ B) : A → B := λ t, x0 + ∫ s in t0..t, (f s (x s)) ∂μ
 
 lemma P_raw.continuous 
 : ∀ (x : A →ᵇ B), continuous (P_raw x) :=
@@ -48,35 +49,77 @@ sorry
 def P : (A →ᵇ B) → (A →ᵇ B) :=
 λ x, ⟨P_raw x, ⟨P_raw.continuous x, P_raw.bounded x⟩⟩ 
 
+@[simp] lemma P_fn (x : A →ᵇ B) (t : A) 
+: P x t = x0 + ∫ s in t0..t, (f s (x s)) ∂μ := rfl
+
 -- TODO: Move.
+private lemma ennreal.continuous_at_of_real : ∀ x, continuous_at ennreal.of_real x := 
+λ x, (continuous_iff_continuous_at.1 ennreal.continuous_of_real x)
+
+private lemma ennreal.monotone_of_real : monotone ennreal.of_real :=
+λ a b, ennreal.of_real_le_of_real
+
 private lemma ennreal.of_real_supr 
 {ι : Type*} [nonempty ι] (f : ι → ℝ) (hbdd : bdd_above (range f))
 : ennreal.of_real (supr f) = supr (λ t, ennreal.of_real (f t)) := 
 begin
-    have hcts : continuous_at ennreal.of_real (supr f),
-    { exact (continuous_iff_continuous_at.1 ennreal.continuous_of_real _), },
-    have hmono : monotone ennreal.of_real,
-    { intros a b, exact ennreal.of_real_le_of_real, },
+    have hcts := ennreal.continuous_at_of_real (supr f),
+    have hmono := ennreal.monotone_of_real,
     exact (map_csupr_of_continuous_at_of_monotone hcts hmono hbdd),
 end 
 
 open bounded_continuous_function
 
+-- TODO: Move: conditionally_complete_lattice
+private lemma le_csupr_iff.mpr
+{α : Type*} {ι : Sort*} [conditionally_complete_lattice α] 
+{s : ι → α} {a : α} (hs : bdd_above (range s)) (h : ∀ (b : α), (∀ (i : ι), s i ≤ b) → a ≤ b)
+: a ≤ supr s := 
+h (supr s) (λ i, le_csupr hs i)
+
+-- TODO: Prove something of the form:
+-- bdd_above (range (λ (t : A), dist (⇑(P x) t) (⇑(P y) t))).
+
 lemma P.edist_eq_supr (x y : A →ᵇ B) 
 : edist (P x) (P y) = supr (λ t, edist (P x t) (P y t)) :=
 begin
-    --unfold edist, unfold metric_space.edist, 
-    apply le_antisymm,
-    { have := λ t, @dist_coe_le_dist A B _ _ x y t, },
-    { sorry, },
+    unfold edist, unfold metric_space.edist,
+    let S := {C : ℝ | 0 ≤ C ∧ ∀ (x_1 : A), dist ((P x) x_1) ((P y) x_1) ≤ C},
+    show ennreal.of_real (Inf S) = _,
+    have hS : S.nonempty, 
+    { use [supr (λ t, dist ((P x) t) ((P y) t))], split,
+      { eapply le_csupr_iff.mpr,
+        { sorry, },
+        { intros b h,
+          replace h := h (nonempty.some (by apply_instance)),
+          exact (le_trans dist_nonneg h), }, },
+      { apply le_csupr,
+        { sorry, }, }, },
+    have hSbdd : bdd_below S,
+    { use 0, intros x hx, exact hx.1, },
+    have h := map_cInf_of_continuous_at_of_monotone 
+        (ennreal.continuous_at_of_real (Inf S)) ennreal.monotone_of_real hS hSbdd,
+    rw h, -- Is this even useful?
+    sorry,
 end 
 
 parameters (K : nnreal) (hK : K < 1)
 
+#check interval_integral.integral_sub
+
 lemma P.lipschitz_at_of_lipshitz (hf : ∀ s, lipschitz_with K (f s)) (t : A)
 : lipschitz_with K (λ x, P x t) :=
 begin 
-    intros x y, simp, sorry,
+    intros x y, simp, unfold edist, unfold metric_space.edist,
+    rw metric_space.edist_dist, cases K with K hK,
+    rw ←ennreal.of_real_eq_coe_nnreal,
+    rw ←ennreal.of_real_mul hK,
+    apply ennreal.of_real_le_of_real,
+    rw dist_eq_norm, 
+    calc ∥x0 + ∫ (s : A) in t0..t, f s (x s) ∂μ - (x0 + ∫ (s : A) in t0..t, f s (y s) ∂μ)∥ 
+        = ∥∫ (s : A) in t0..t, f s (x s) ∂μ - ∫ (s : A) in t0..t, f s (y s) ∂μ∥ : begin sorry, end
+    ... = ∥∫ (s : A) in t0..t, (f s (x s)) -  (f s (y s)) ∂μ∥ : begin sorry, end
+    ... ≤ K * Inf {C : ℝ | 0 ≤ C ∧ ∀ (x_1 : A), dist (x x_1) (y x_1) ≤ C} : sorry
 end
 
 lemma P.lipschitz_of_lipschitz (hf : ∀ s, lipschitz_with K (f s))
@@ -88,9 +131,11 @@ begin
     ... ≤ ↑K * edist x y : supr_le (λ t, P.lipschitz_at_of_lipshitz hf t x y),
 end 
 
-def P.efixed_point : A →ᵇ B := 
-contracting_with.efixed_point P ⟨hK, P.lipschitz⟩ sorry sorry
+--lemma P.edist_lt_top : Π (x : A →ᵇ B), edist x (P x) < ⊥ := sorry
 
-#check contracting_with.efixed_point
+def P.efixed_point_of_lipschitz (hf : ∀ s, lipschitz_with K (f s)) : A →ᵇ B := 
+contracting_with.efixed_point P ⟨hK, P.lipschitz_of_lipschitz hf⟩ sorry sorry
+
+#check contracting_with.efixed_point P
 
 end picard_operator
