c7149c4bbe944425eeef64e8a08da51b2344804c
Ramon Fernandez Mir <rfernandezmir@gmail.com>
2020-11-19 20:03:23 +0000

    Very close to proving Lipschitz condition.

--- src/picard_lindelof/picard_operator.lean
+++ src/picard_lindelof/picard_operator.lean
@@ -105,20 +105,55 @@ end
 
 parameters (K : nnreal) (hK : K < 1)
 
-#check interval_integral.integral_sub
-
-lemma P.lipschitz_at_of_lipshitz (hf : ∀ s, lipschitz_with K (f s)) (t : A)
-: lipschitz_with K (λ x, P x t) :=
+-- TODO: Move.
+private lemma norm_integral_le_integral_norm_Ioc_of_le
+{α E : Type*} [linear_order α] [measurable_space α]
+[measurable_space E] [normed_group E]
+[second_countable_topology E] [complete_space E] [normed_space ℝ E] [borel_space E]
+{a b : α} {f : α → E} {μ : measure α} (h : a ≤ b)
+: ∥∫ x in a..b, f x ∂μ∥ ≤ ∫ x in a..b, ∥f x∥ ∂μ :=
 begin 
-    intros x y, simp, unfold edist, unfold metric_space.edist,
+    rw [integral_of_le h, integral_of_le h],
+    have hle := @norm_integral_le_integral_norm_Ioc α E _ _ _ _ _ _ _ _ a b f μ,
+    rw [integral_of_le h, max_eq_right h, min_eq_left h] at hle,
+    exact hle,
+end 
+
+--private lemma interval_integral.integral_mono
+s
+lemma P.lipschitz_at_of_lipshitz 
+(hf : ∀ s, lipschitz_with K (f s)) (t : A) (ht : t0 ≤ t) (x y : A →ᵇ B) 
+(hx : interval_integrable (λ t, f t (x t)) μ t0 t) (hy : interval_integrable (λ t, f t (y t)) μ t0 t)
+: edist (P x t) (P y t) ≤ ↑K * edist x y :=
+begin 
+    simp, unfold edist, unfold metric_space.edist,
     rw metric_space.edist_dist, cases K with K hK,
     rw ←ennreal.of_real_eq_coe_nnreal,
     rw ←ennreal.of_real_mul hK,
     apply ennreal.of_real_le_of_real,
     rw dist_eq_norm, 
-    calc ∥x0 + ∫ (s : A) in t0..t, f s (x s) ∂μ - (x0 + ∫ (s : A) in t0..t, f s (y s) ∂μ)∥ 
-        = ∥∫ (s : A) in t0..t, f s (x s) ∂μ - ∫ (s : A) in t0..t, f s (y s) ∂μ∥ : begin sorry, end
-    ... = ∥∫ (s : A) in t0..t, (f s (x s)) -  (f s (y s)) ∂μ∥ : begin sorry, end
+    calc ∥x0 + ∫ s in t0..t, f s (x s) ∂μ - (x0 + ∫ s in t0..t, f s (y s) ∂μ)∥ 
+        = ∥∫ s in t0..t, f s (x s) ∂μ - ∫ s in t0..t, f s (y s) ∂μ∥
+        : by rw [sub_eq_add_neg, neg_add, add_comm x0, add_assoc, ←add_assoc x0, 
+                 add_comm x0, neg_add_self, zero_add, ←sub_eq_add_neg]
+    ... = ∥∫ s in t0..t, (f s (x s)) - (f s (y s)) ∂μ∥ 
+        : by rw interval_integral.integral_sub hx hy
+    ... ≤ ∫ s in t0..t, ∥(f s (x s)) - (f s (y s))∥ ∂μ
+        : norm_integral_le_integral_norm_Ioc_of_le ht
+    ... = ∫ s in t0..t, (dist (f s (x s)) (f s (y s))) ∂μ
+        : by congr; ext; exact (dist_eq_norm _ _).symm
+    ... ≤ ∫ s in t0..t, K * dist (x s) (y s) ∂μ
+        : begin 
+            have hxsys := λ s, hf s (x s) (y s),
+            -- TODO: Factor this out. Argument about edist dist and le.
+            have hrw : (λ s, dist (f s (x s)) (f s (y s))) ≤ (λ s, K * dist (x s) (y s)) := sorry,
+            -- This follows from interval_integral.integral_mono.
+            sorry
+         end
+         -- Then we use norm_integral_le_of_norm_le_const
+         -- Bound it above by the supremum!
+         -- Well have something like K * (t - t0) * s ≤ K * s 
+         -- So we play with the t-t0 and K's and we should be good.
     ... ≤ K * Inf {C : ℝ | 0 ≤ C ∧ ∀ (x_1 : A), dist (x x_1) (y x_1) ≤ C} : sorry
 end
 
@@ -128,7 +163,7 @@ begin
     intros x y,
     calc edist (P x) (P y) 
         = supr (λ t, edist (P x t) (P y t)) : P.edist_eq_supr x y
-    ... ≤ ↑K * edist x y : supr_le (λ t, P.lipschitz_at_of_lipshitz hf t x y),
+    ... ≤ ↑K * edist x y : sorry --supr_le (λ t, P.lipschitz_at_of_lipshitz hf t x y),
 end 
 
 --lemma P.edist_lt_top : Π (x : A →ᵇ B), edist x (P x) < ⊥ := sorry
