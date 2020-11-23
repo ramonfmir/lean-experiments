bbd42a0ee512da381d8fd637f73e937419749dad
Ramon Fernandez Mir <rfernandezmir@gmail.com>
2020-11-23 18:20:24 +0000

    P.to_fun.continuous_on working again.

--- src/picard_lindelof/definitions.lean
+++ src/picard_lindelof/definitions.lean
@@ -14,8 +14,9 @@ structure locally_bounded_real_continuous_function (A : Type*)
 [normed_space ℝ A] [complete_space A] [second_countable_topology A] :=
 (to_fun : ℝ → A)
 (t₀ t₁ : ℝ) (ht : t₀ ≤ t₁)
-(hdom : function.support to_fun ⊆ Icc t₀ t₁)
+--(hdom : function.support to_fun ⊆ Icc t₀ t₁)
 (hcts : continuous_on to_fun (Icc t₀ t₁))
+-- Should it be bounded?
 
 namespace picard_operator
 
@@ -30,7 +31,7 @@ structure initial_value_problem (x : C(E)) :=
 (x₀ : E) (v : ℝ → E → E)
 (K : nnreal) (hK : K < 1)
 (hlipschitz : ∀ s, lipschitz_with K (v s))
-(hbdd : ∃ C, 0 < C ∧ ∀ t ∈ Ico x.t₀ x.t₁, ∥v t (x t)∥ ≤ C)
+(hbdd : ∃ C, 0 < C ∧ ∀ t ∈ Ioc x.t₀ x.t₁, ∥v t (x t)∥ ≤ C)
 (hintegrable : interval_integrable (λ s, v s (x s)) volume x.t₀ x.t₁)
 
 notation `IVP(` x `)` := initial_value_problem x
@@ -71,33 +72,44 @@ begin
     rw metric.continuous_on_iff,
     intros a ha ε hε, use [ε/C, div_pos hε hCpos],
     intros b hb hab, rw [P.to_fun.dist_eq x I],
-    have hboundab : ∀ s, s ∈ Ico (min b a) (max b a) → ∥I.v s (x s)∥ ≤ C,
+    have hboundab : ∀ s, s ∈ Ioc (min b a) (max b a) → ∥I.v s (x s)∥ ≤ C,
     { by_cases (a ≤ b),
       { rw [min_eq_right h, max_eq_left h], 
         intros s hs, apply (hC s), 
-        --exact ⟨le_trans ha.1 hs.1, le_of_lt (lt_of_lt_of_le hs.2)⟩,
-        sorry, },
+        have hlb := lt_of_le_of_lt ha.1 hs.1,
+        have hub := le_trans hs.2 hb.2,
+        exact ⟨hlb, hub⟩, },
       { rw [min_eq_left (le_of_not_le h), max_eq_right (le_of_not_le h)], 
-        intros s hs, sorry, }, },
-    -- have hbound := interval_integral.norm_integral_le_of_norm_le_const hboundab,
-    -- rw [dist_eq_norm, norm_eq_abs] at hab,
-    -- replace hab := mul_lt_mul_of_pos_left hab hCpos,
-    -- rw [←mul_div_assoc, mul_div_cancel_left ε (ne_of_lt hCpos).symm, abs_sub] at hab,
-    -- exact lt_of_le_of_lt hbound hab,
+        intros s hs, apply (hC s),
+        replace h := le_of_not_le h,
+        have hlb := lt_of_le_of_lt hb.1 hs.1,
+        have hub := le_trans hs.2 ha.2,
+        exact ⟨hlb, hub⟩, }, },
+    have hbound := interval_integral.norm_integral_le_of_norm_le_const hboundab,
+    rw [dist_eq_norm, norm_eq_abs] at hab,
+    replace hab := mul_lt_mul_of_pos_left hab hCpos,
+    rw [←mul_div_assoc, mul_div_cancel_left ε (ne_of_lt hCpos).symm, abs_sub] at hab,
+    exact lt_of_le_of_lt hbound hab,
 end
 
 -- The Picard operator is bounded.
-lemma P.to_fun.bounded : ∃ C, ∀ (a b : ℝ), dist (P.to_fun a) (P.to_fun b) ≤ C := 
+lemma P.to_fun.bounded (x : C(E)) (I : IVP(x))
+: ∃ C, ∀ (a b : ℝ), dist (P.to_fun x I a) (P.to_fun x I b) ≤ C := 
 begin
     sorry,
 end
 
 -- Picard operator.
-def P : (ℝ →ᵇ E) → (ℝ →ᵇ E) :=
-λ x, ⟨P.to_fun x, ⟨P.to_fun.continuous x, P.to_fun.bounded x⟩⟩ 
+def P (x : C(E)) (I : IVP(x)) : C(E) := {
+    to_fun := P.to_fun x I,
+    t₀ := x.t₀,
+    t₁ := x.t₁,
+    ht := x.ht,
+    hcts := P.to_fun.continuous_on x I,
+}
 
-@[simp] lemma P.def (x : ℝ →ᵇ E) (t : ℝ) 
-: P x t = x₀ + ∫ s in t₀..t, v s (x s) ∂μ := rfl
+@[simp] lemma P.def (x : C(E)) (I : IVP(x)) (t : ℝ)
+: P x I t = I.x₀ + ∫ s in x.t₀..t, I.v s (x s) := rfl
 
 -- Initial value problem.
 parameters (x : locally_bounded_continuous_function)
