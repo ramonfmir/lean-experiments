ff4de1cc6bf9c086c7a2dd0adc47f54e0fdd31c3
Ramon Fernandez Mir <rfernandezmir@gmail.com>
2020-12-03 19:35:18 +0000

    More progress on FTC-2.

--- src/picard_lindelof/other/ftc2.lean
+++ src/picard_lindelof/other/ftc2.lean
@@ -73,19 +73,22 @@ variables {f' g : ℝ → E}
 -- end
 
 theorem eq_of_deriv_eq
-  (hfderiv : ∀ x ∈ Icc a b, has_deriv_within_at f (f' x) (Icc a b) x)
-  (hgderiv : ∀ x ∈ Icc a b, has_deriv_within_at g (f' x) (Icc a b) x)
+  (contf : continuous_on f (Icc a b)) 
+  (contg : continuous_on g (Icc a b))
+  (hfderiv : ∀ x ∈ Ico a b, has_deriv_within_at f (f' x) (Ioi x) x)
+  (hgderiv : ∀ x ∈ Ico a b, has_deriv_within_at g (f' x) (Ioi x) x)
   (hi : f a = g a) :
-  ∀ y ∈ Icc a b, f y = g y :=
+  ∀ y ∈ Ico a b, f y = g y :=
 begin
-  have hzero : ∀ z ∈ Icc a b, has_deriv_within_at (f - g) 0 (Icc a b) z,
+  have hzero : ∀ z ∈ Ico a b, has_deriv_within_at (f - g) 0 (Ioi z) z,
   { intros z hz,
     convert has_deriv_within_at.sub (hfderiv z hz) (hgderiv z hz),
     rw sub_self, },
   have hbound : ∀ z ∈ Ico a b, ∥(0 : E)∥ ≤ 0 
     := λ _ _, by rw norm_le_zero_iff,
   intros y hy,
-  have hnormle := norm_image_sub_le_of_norm_deriv_le_segment' hzero hbound y hy,
+  have hnormle := norm_image_sub_le_of_norm_deriv_right_le_segment 
+    (contf.sub contg) hzero hbound y (mem_Icc_of_Ico hy),
   simpa [zero_mul, norm_le_zero_iff, sub_eq_zero, sub_eq_zero.mpr hi] using hnormle,
 end
 
@@ -99,47 +102,66 @@ lemma interval_integrable_right
   ∀ x ∈ Icc a b, interval_integrable f' volume a x := 
 sorry
 
-lemma deriv_integral 
+#check continuous_within_at_iff_continuous_at_restrict
+#check has_deriv_within_at_iff_tendsto_slope'
+
+lemma deriv_integral_right
   (contf' : continuous f')
-  (derivf : ∀ x ∈ Icc a b, has_deriv_within_at f (f' x) (Icc a b) x) 
+  (derivf : ∀ x ∈ Ico a b, has_deriv_within_at f (f' x) (Ioi x) x) 
   (intgf' : interval_integrable f' volume a b) :
-  ∀ x ∈ Icc a b, 
-  has_deriv_within_at (λ u, ∫ y in a..u, f' y) (f' x) (Icc a b) x :=
+  ∀ x ∈ Ico a b, 
+  has_deriv_within_at (λ u, ∫ y in a..u, f' y) (f' x) (Ioi x) x :=
 begin 
   intros x hx,
-  have intgf'l := interval_integrable_left intgf' x hx,
-  have intgf'r := interval_integrable_right intgf' x hx,
-
-  --have h := @integral_has_deriv_within_at_right, 
-    --_ _ _ _ _ _ _ _ _ _ intgf', --_ (Ioi a) _
-    --(continuous.continuous_within_at contf'),
-
+  have intgf'r := interval_integrable_right intgf' x (mem_Icc_of_Ico hx),
   have hderivci := @integral_has_deriv_within_at_right 
     _ _ _ _ _ _ _ _ _ _ intgf'r _ _ (FTC_filter.nhds_right x)
     (continuous.continuous_within_at contf'),
-  have hderivic := @integral_has_deriv_within_at_left 
-    _ _ _ _ _ _ _ _ _ _ intgf'l _ _ (FTC_filter.nhds_left x)
-    (continuous.continuous_within_at contf'),
   have hderivxb := has_deriv_within_at.mono hderivci (@Icc_subset_Ici_self _ _ x b),
-  have hderivax := has_deriv_within_at.mono hderivic (@Icc_subset_Iic_self _ _ a x),
-  
-  --refine has_deriv_within_at.nhds_within hderivxb _,
-
-  sorry,
+  refine has_deriv_within_at.nhds_within hderivxb _,
+  apply Icc_mem_nhds_within_Ioi,
+  exact ⟨le_refl x, hx.2⟩,
 end 
+
+#check continuous_within_at_singleton
 
 theorem ftc2
   (contf : continuous_on f (Icc a b)) 
   (contf' : continuous f')
-  (derivf : ∀ x ∈ Icc a b, has_deriv_within_at f (f' x) (Icc a b) x)
+  (derivf : ∀ x ∈ Ico a b, has_deriv_within_at f (f' x) (Ioi x) x)
   (intgf' : interval_integrable f' volume a b) :
-  ∀ x ∈ Icc a b, ∫ y in a..x, f' y = f x - f a :=
+  ∀ x ∈ Ico a b, ∫ y in a..x, f' y = f x - f a :=
 begin
     intros x hx, 
     apply eq_sub_of_add_eq, symmetry,
-    refine (eq_of_deriv_eq derivf _ _) x hx,
+    have derivint : ∀ z ∈ Ico a b, 
+      has_deriv_within_at (λ u, (∫ y in a..u, f' y) + f a) (f' z) (Ioi z) z,
     { intros y hy, apply has_deriv_within_at.add_const,
-      exact (deriv_integral contf' derivf intgf' y hy), },
+      exact (deriv_integral_right contf' derivf intgf' y hy), },
+    refine (eq_of_deriv_eq contf _ derivf derivint _) x hx,
+    { intros z hz,
+      by_cases hab : b ≤ a, { rw Ico_eq_empty hab at hx, cases hx, },
+      replace hab := lt_of_not_ge hab,
+      by_cases hzb : z < b,
+      { have derivinta := derivint z ⟨hz.1, hzb⟩,
+        have hctsoi := has_deriv_within_at.continuous_within_at derivinta,
+        have hctsoc := continuous_within_at.mono hctsoi (@Ioc_subset_Ioi_self _ _ z b),
+        -- Plan:
+        -- 1. deriv left. 
+        -- 2. [a, b] = [a, z) ∪ {z} ∪ (z, b] 
+        -- 3. Split on union:
+        --     * first corresponds to left, 
+        --     * middle corresponds to continuous_within_at_singleton,
+        --     * last corresponds to right.
+
+        --rw ←Ioc_union_left (le_of_lt hab),
+        --refine continuous_within_at_union.2 ⟨_, _⟩,
+        --convert hctsoc,
+        --{ exact hctsoc, },
+        --{ exact continuous_within_at_singleton, }, 
+      },
+      { sorry, },
+      },
     { simp only [integral_same, zero_add], },
 end 
 
