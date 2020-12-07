4197c9b6cf69cfa55ae781c5743f6ece0df0d7ca
Ramon Fernandez Mir <rfernandezmir@gmail.com>
2020-12-07 14:44:57 +0000

    Euler's method and Taylor's formula follow paper.

--- src/ODE_enclosures/euler_method.lean
+++ src/ODE_enclosures/euler_method.lean
@@ -1,8 +1,10 @@
 import data.real.basic
 import analysis.calculus.times_cont_diff
+import topology.bounded_continuous_function
+import measure_theory.interval_integral
+import topology.metric_space.basic
 
-import picard_lindelof.picard_lindelof
-import picard_lindelof.domain
+import ODE_enclosures.taylors_theorem
 
 noncomputable theory
 
@@ -10,6 +12,13 @@ open metric set asymptotics filter real measure_theory
 open interval_integral topological_space uniform_space
 
 open_locale topological_space classical filter uniformity 
+
+section euler_method 
+
+local infix ` →ᵇ `:25 := bounded_continuous_function 
+
+variables {E : Type*} [measurable_space E] [normed_group E] [borel_space E] [linear_order E]
+                      [normed_space ℝ E] [complete_space E] [second_countable_topology E]
 
 -- fun rk_eval :: "(nat⇒nat⇒real) ⇒ (nat⇒real) ⇒ (real×'a::real_vector ⇒ 'a) ⇒ real ⇒ real ⇒ 'a ⇒ nat ⇒ 'a" where
 --   "rk_eval A c f t h x j =
@@ -35,9 +44,8 @@ open_locale topological_space classical filter uniformity
 -- definition euler_increment where
 --   "euler_increment f = rk_increment f 1 (λi j. 0) (λi. 1) (λi. 0)"
 
---   = (rk_eval (*0) (*0) f t h x 1)"
-
---   = f (t, x)"
+def euler_step (f : ℝ → E → E) (x : ℝ →ᵇ E) (h : ℝ) (t : ℝ) : E := 
+(x t) + h • (f t (x t))
 
 -- definition euler where
 --   "euler f = grid_function (discrete_evolution (euler_increment f))"
@@ -73,42 +81,35 @@ open_locale topological_space classical filter uniformity
 --   assumes lipschitz_area: "⋀t. t ∈ I ⟹ cball (solution t) ¦r¦ ⊆ D'"
 -- begin
 
+-- THEOREM 4 
 -- lemma euler_consistent_solution:
 --   fixes t'
 --   assumes t': "t' ∈ I"
 --   shows "consistent solution t' T B' 1 (euler_increment f)"
 
-section euler_method 
-
-local infix ` →ᵇ `:25 := bounded_continuous_function 
-
--- NOTE: This is meant to be ℝ^n.
-variables {E : Type*} [measurable_space E] [normed_group E] [borel_space E] [linear_order E]
-                      [normed_space ℝ E] [complete_space E] [second_countable_topology E]
-
-def euler_step (f : ℝ → E → E) (x : ℝ →ᵇ E) (h : ℝ) (t : ℝ) : E := 
-(x t) + h • (f t (x t))
+-- MISSING: Taylor's formula.
 
 lemma euler_step_consistent 
-  (f : ℝ → E → E) (x : ℝ →ᵇ E) (h : ℝ) (t : ℝ)
+  -- Should be 
+  --    (f : ℝ → E → E) (x : ℝ →ᵇ E) (h : ℝ) (t : ℝ)
+  -- but my statement of Taylor's formula is on fns ℝ → ℝ.
+  (f : ℝ → ℝ → ℝ) (x : ℝ →ᵇ ℝ) (h : ℝ) (hpos : 0 < h) (t : ℝ)
   -- Initial value problem: x'(t) = f(t,x(t)).
+  (hdiff : ∀ t, times_cont_diff_at ℝ 2 x t)
   (hf : ∀ t, has_deriv_at x (f t (x t)) t)
-  -- Assume the derivative is bounded.
-  (C : nnreal) (hbdd : ∀ t, ∥f t (x t)∥ ≤ C)
+  -- Assume the derivative (of f) is bounded.
+  (C : nnreal) (hbdd : ∀ s, ∥deriv (λ t, f t (x t)) s∥ ≤ C)
   -- Assume it is Lipschitz on the second argument? 
   (K : nnreal) (hltz : ∀ s, lipschitz_with K (f s))
   : dist (f (t + h) (x (t + h))) (euler_step f x h t) ≤ C * h^2 / 2 :=
 begin
-  have evox : ftaylor_series ℝ x (t + h) 2 = 0,
-  { unfold ftaylor_series, ext m,
-    rw iterated_fderiv_succ_apply_right,
-    rw iterated_fderiv_succ_apply_right,
-    rw iterated_fderiv_zero_apply, simp,
-
-    sorry, },
+  have h := taylor_left x.1 t 1 (hdiff t) (t + h) ((lt_add_iff_pos_right t).2 hpos),
+  rcases h with ⟨c, hc1, hc2, h⟩,
+  -- Proof follows from here.
   sorry,
 end 
 
+-- Consistency implies convergence of:
 noncomputable def euler.recursive (f : ℝ → E → E) (x : ℝ → E) (h : ℝ) (t : ℝ) : ℕ → E
 | 0     := x t
 | (n+1) := (euler.recursive n) + h • (f (t + h) (euler.recursive n))
--- src/ODE_enclosures/taylors_theorem.lean
+++ src/ODE_enclosures/taylors_theorem.lean
@@ -1,39 +1,41 @@
 import analysis.calculus.mean_value
 import analysis.calculus.times_cont_diff
 import analysis.calculus.iterated_deriv
+import measure_theory.measurable_space
+import measure_theory.borel_space
+import topology.basic
 
-open nat finset
+open nat finset topological_space
 
 open_locale big_operators
+
+variables {E : Type*} [measurable_space E] [normed_group E] [borel_space E] [linear_order E]
+                      [normed_space ℝ E] [complete_space E] [second_countable_topology E]
 
 @[reducible] noncomputable def R (n : ℕ) (f : ℝ → ℝ) (a : ℝ) : ℝ → ℝ := 
 λ x, (iterated_deriv (n + 1) f a) * (x - a)^(n + 1) / factorial (n + 1)
 
 @[reducible] noncomputable def taylor (n : ℕ) (f : ℝ → ℝ) (a : ℝ) : ℝ → ℝ := 
-λ x, ∑ i in range n, (iterated_deriv i f a) * (x - a)^i / factorial i
+λ x, ∑ i in range (n + 1), (iterated_deriv i f a) * (x - a)^i / factorial i
 
-lemma taylor_zero (f : ℝ → ℝ) (a : ℝ) : taylor 0 f a = λ _, 0 := 
-by simp [taylor]
-
-lemma taylor_one (f : ℝ → ℝ) (a : ℝ) : taylor 1 f a = λ _, f a := 
+lemma taylor_zero (f : ℝ → ℝ) (a : ℝ) : taylor 0 f a = λ _, f a := 
 by simp [taylor]
 
 lemma taylor_succ (f : ℝ → ℝ) (a : ℝ) (n : ℕ) (x : ℝ)
-: taylor (n + 1) f a x = taylor n f a x + iterated_deriv n f a * (x - a) ^ n / n.factorial := 
-by simp [taylor, finset.sum_range_succ, add_comm]
+: taylor (n + 1) f a x = 
+taylor n f a x + iterated_deriv (n + 1) f a * (x - a) ^ (n + 1) / factorial (n + 1) := 
+sorry
 
 lemma deriv_taylor_eq
   (f : ℝ → ℝ) (n : ℕ) (a : ℝ) (x : ℝ) :
-  deriv (taylor n f a) x = taylor n (deriv f) a x :=
+  deriv (taylor (n + 1) f a) x = taylor n (deriv f) a x :=
 begin 
   induction n with n hn generalizing a x,
-  { simp [taylor_zero], },
+  { simp [taylor_zero, finset.sum_range_succ], },
   { simp [taylor_succ], 
     rw [←hn a x, finset.sum_range_succ', ←iterated_deriv_succ'],
     simp only [factorial_succ], simp, 
-    have h1 : (x - a) ^ n = (x - a) ^ ((n + 1) - 1) := by congr,
-    have h2 : n.factorial = ((n + 1) - 1).factorial := by congr,
-    rw [h1, h2, add_comm], sorry, },
+    sorry, },
 end 
 
 theorem taylor_left 
@@ -44,4 +46,5 @@ theorem taylor_left
        f x = taylor n f a x + R n f c x :=
 begin 
   intros x hx,
+  sorry,
 end
